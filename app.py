d8f971112f85b7be836fe3fff3eea3775ad37abf
Jacques Kvam <jwkvam@gmail.com>
2017-04-10 11:42:22 -0700

    clean app a bit

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+import sys
+
 from flask import Flask
+import tensorflow as tf
+
 app = Flask(__name__)
 app.debug = True
 
-import sys
-
 print(sys.maxunicode)
-print(os.system('ls -l'))
-
-import tensorflow as tf
-import numpy as np
-import pandas as pd
 
 @app.route('/')
 def index():
-    print(os.system('cd /tmp && du -hd 1'))
-    print(os.system('ls -larth /tmp'))
     x = tf.ones(5) * 2
     with tf.Session():
         y = x.eval()
-    print(pd.__version__)
     return "Hello, world!" + str(y), 200
 
 # We only need this for local development.
